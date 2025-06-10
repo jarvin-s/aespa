19dd0efda5a8e3324ac9505b5b54dbb20983ca02
Jarvinnn <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-06-11 01:27:43 +0200

    feat: enhance quiz components with animations and scoring improvements
    - Update loading indicator with a spinner and adjusted text
    - Refactor answer history to include time to answer and points earned
    - Implement timer for answering questions with visual feedback
    - Improve quiz completion screen with animations and score display

--- src/components/quiz/complete.tsx
+++ src/components/quiz/complete.tsx
@@ -2,22 +2,9 @@ import React from 'react'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
 import { useUser } from '@clerk/nextjs'
-import localFont from 'next/font/local'
-import { CircleDashed } from 'lucide-react'
 import { CardContent } from '../ui/card'
-import { Card } from '../ui/card'
 import { Share2 } from 'lucide-react'
-import { Roboto } from 'next/font/google'
-
-const roboto = Roboto({
-    weight: '400',
-    subsets: ['latin'],
-})
-
-const aespaFont = localFont({
-    src: '/../../../public/fonts/aespa_Regular.ttf',
-    display: 'swap',
-})
+import { motion } from 'motion/react'
 
 interface QuizCompleteProps {
     score: number
@@ -25,55 +12,52 @@ interface QuizCompleteProps {
 }
 
 const QuizComplete = ({ score, totalQuestions }: QuizCompleteProps) => {
-    const percentage = (score / totalQuestions) * 100
     const { user } = useUser()
 
     return (
-        <div
-            className={`${roboto.className} quiz-complete flex min-h-screen flex-col items-center justify-center text-white`}
-        >
+        <div className='quiz-complete flex min-h-screen flex-col items-center justify-center text-white'>
             <div className='relative w-full max-w-4xl rounded-md p-4'>
-                <h2
-                    className={`${aespaFont.className} mb-6 text-center text-5xl font-bold text-white md:text-9xl`}
+                <motion.h2
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5 }}
+                    className='mb-6 text-center text-5xl font-bold text-white md:text-9xl'
                 >
                     Quiz complete!
-                </h2>
-                <Card className='mb-6 overflow-hidden border-white/20 bg-purple-500/30 text-white backdrop-blur-md'>
+                </motion.h2>
+
+                <motion.div
+                    initial={{ opacity: 0, y: 20 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 0.5, delay: 0.5 }}
+                    className='mb-6 overflow-hidden rounded-md text-white'
+                >
                     <CardContent className='p-0'>
                         <div className='flex flex-col items-center justify-center p-8'>
                             <div className='relative mb-6'>
-                                <CircleDashed
-                                    className='h-36 w-36 text-white/20'
-                                    strokeWidth={1}
-                                />
-                                <div className='absolute inset-0 flex items-center justify-center'>
-                                    <div className='text-center'>
-                                        <span
-                                            className={`${aespaFont.className} text-5xl font-bold`}
-                                        >
-                                            {percentage}%
-                                        </span>
-                                    </div>
+                                <div className='text-center'>
+                                    <span className='text-5xl font-bold'>
+                                        Well done! You scored{' '}
+                                        <span className='text-purple-500'>
+                                            {score}
+                                        </span>{' '}
+                                        points.
+                                    </span>
                                 </div>
                             </div>
 
-                            <div
-                                className={` ${aespaFont.className} text-center`}
-                            >
+                            <div className='text-center'>
                                 <p className='text-4xl font-bold'>
-                                    {score}{' '}
-                                    <span className='text-white/80'>
-                                        out of
-                                    </span>{' '}
+                                    {(score / 1000).toFixed(0)} out of{' '}
                                     {totalQuestions}
                                 </p>
-                                <p className='mt-2 text-xl text-white/80'>
+                                <p className='mt-2 text-xl'>
                                     questions answered correctly
                                 </p>
                             </div>
                         </div>
 
-                        <div className='flex justify-center border-t border-white/20'>
+                        <div className='flex justify-center'>
                             <div className='flex flex-col items-center justify-center p-6'>
                                 <div className='w-full'>
                                     <Button
@@ -89,25 +73,31 @@ const QuizComplete = ({ score, totalQuestions }: QuizCompleteProps) => {
                                     >
                                         <Share2 className='h-6 w-6 text-white/80' />
                                         <p className='text-sm text-white/80'>
-                                            Share Score
+                                            Share score
                                         </p>
                                     </Button>
                                 </div>
                             </div>
                         </div>
                     </CardContent>
-                </Card>
+                </motion.div>
                 <div className='flex flex-col items-center gap-4'>
-                    <div className='flex flex-col gap-4 md:flex-row'>
+                    <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.5, delay: 1 }}
+                        className='flex flex-col gap-4 md:flex-row'
+                    >
                         {user ? (
                             <>
                                 <Button
                                     asChild
                                     variant='outline'
-                                    disabled={true}
                                     className='w-64 border-[#6d6d6d2a] text-white hover:bg-purple-700'
                                 >
-                                    <Link href='/leaderboard'>Leaderboard</Link>
+                                    <Link href='/leaderboard'>
+                                        Check out the leaderboard
+                                    </Link>
                                 </Button>
                                 <Button
                                     asChild
@@ -128,7 +118,7 @@ const QuizComplete = ({ score, totalQuestions }: QuizCompleteProps) => {
                                 <Link href='/'>Back to Home</Link>
                             </Button>
                         )}
-                    </div>
+                    </motion.div>
                 </div>
             </div>
         </div>
