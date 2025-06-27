70b5903ab7ffa047aeb00ed01cce4e42b298b062
Jarvin <Jarvin.Siegers@Student.GildeOpleidingen.nl>
2025-06-27 22:14:13 +0200

    feat: add filters to photocards collection

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -4,7 +4,6 @@ import Link from 'next/link'
 import { cn } from '@/lib/utils'
 import { motion } from 'framer-motion'
 import {
-    PlusSquare,
     Trophy,
     Image as ImageIcon,
     LayoutDashboard,
@@ -12,9 +11,11 @@ import {
     X,
     ChevronsRight,
     ChevronsLeft,
+    MessageCircleQuestionIcon,
 } from 'lucide-react'
 import { useState, useEffect } from 'react'
 import { usePathname } from 'next/navigation'
+import Image from 'next/image'
 
 export function Sidebar() {
     const [isCollapsed, setIsCollapsed] = useState(false)
@@ -27,9 +28,14 @@ export function Sidebar() {
 
     const navItems = [
         {
+            href: '/dashboard',
+            icon: LayoutDashboard,
+            label: 'Dashboard',
+        },
+        {
             href: '/quiz',
-            icon: PlusSquare,
-            label: 'Create quiz',
+            icon: MessageCircleQuestionIcon,
+            label: 'Quiz',
         },
         {
             href: '/leaderboard',
@@ -41,39 +47,44 @@ export function Sidebar() {
             icon: ImageIcon,
             label: 'Photocards',
         },
-        {
-            href: '/dashboard',
-            icon: LayoutDashboard,
-            label: 'Dashboard',
-        },
     ]
 
     return (
         <>
             <button
-                onClick={() => setIsMobileOpen(!isMobileOpen)}
+                onClick={() => {
+                    setIsMobileOpen(!isMobileOpen)
+                    if (!isMobileOpen) {
+                        setIsCollapsed(false)
+                    }
+                }}
                 className='fixed top-4 left-4 z-50 rounded-lg p-2 text-white backdrop-blur-lg transition-all hover:text-white/60 md:hidden'
             >
                 {isMobileOpen ? <X size={24} /> : <Menu size={24} />}
             </button>
             <div
                 className={cn(
-                    'fixed top-0 left-0 z-40 h-screen flex-col bg-purple-700 transition-all duration-300 md:flex',
+                    'fixed top-0 left-0 z-40 h-screen flex-col bg-purple-700/10 backdrop-blur-[10px] transition-all duration-300 md:flex',
                     isMobileOpen ? 'flex' : 'hidden md:flex',
-                    isCollapsed ? 'w-auto' : 'w-64'
+                    isCollapsed ? 'w-auto' : 'w-[360px] md:w-[300px]'
                 )}
             >
-                <div className='absolute top-4 left-4 hidden items-center px-4 md:flex'>
+                <div className='absolute top-4 left-28 items-center px-4 md:left-16 md:flex'>
                     {!isCollapsed && (
                         <span className='text-lg font-bold text-white'>
-                            aespa quiz
+                            <Image
+                                src='/images/logo.png'
+                                alt='aespa logo'
+                                width={125}
+                                height={125}
+                            />
                         </span>
                     )}
                 </div>
                 <button
                     onClick={() => setIsCollapsed(!isCollapsed)}
                     className={cn(
-                        'absolute top-4 hidden cursor-pointer p-2 text-white transition-all duration-150 hover:bg-gray-200/20 md:block',
+                        'absolute top-4 hidden cursor-pointer rounded-md p-2 text-white transition-all duration-150 hover:bg-gray-200/20 md:block',
                         isCollapsed ? 'right-1/2 translate-x-1/2' : 'right-2'
                     )}
                 >
@@ -84,7 +95,7 @@ export function Sidebar() {
                     )}
                 </button>
 
-                <div className='mt-16 flex flex-col gap-2 p-4'>
+                <div className='mt-24 flex flex-col gap-2 p-4'>
                     {navItems.map((item) => {
                         const isActive = pathname === item.href
                         const Icon = item.icon
